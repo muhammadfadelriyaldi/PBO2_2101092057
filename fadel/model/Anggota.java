67a95694351954ba8bffd01fb15f6400c1b9dcdf
muhammadfadelriyaldi <muhammadfadhelriyaldi@gmail.com>
2023-03-09 16:00:33 +0700

    Create Anggota.java

---
+++ fadel/model/Anggota.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fadel.model;
+
+/**
+ *
+ * @author Muhammad Fadel R
+ */
+public class Anggota {
+    private String kodeAnggota;
+    private String namaAnggota;
+    private String alamat;
+    private String jenisKelamin;
+    
+    public Anggota() {
+    }
+
+    public Anggota(String kodeAnggota, String namaAnggota, String alamat, String jenisKelamin) {
+        this.kodeAnggota = kodeAnggota;
+        this.namaAnggota = namaAnggota;
+        this.alamat = alamat;
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getKodeAnggota() {
+        return kodeAnggota;
+    }
+
+    public void setKodeAnggota(String kodeAnggota) {
+        this.kodeAnggota = kodeAnggota;
+    }
+
+    public String getNamaAnggota() {
+        return namaAnggota;
+    }
+
+    public void setNamaAnggota(String namaAnggota) {
+        this.namaAnggota = namaAnggota;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+    
+    
+}
