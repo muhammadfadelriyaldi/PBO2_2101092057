2f2813a6a1dee4ead401bf63cbc08940c28f11ac
muhammadfadelriyaldi <muhammadfadhelriyaldi@gmail.com>
2023-03-09 16:00:28 +0700

    Create Koneksi.java

---
+++ fadel/dao/Koneksi.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fadel.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Muhammad Fadel R
+ */
+public class Koneksi {
+    private String url = "jdbc:mysql://localhost/dbpbo2_2101092057";
+    private String username ="root";
+    private String password="";
+   
+   public Connection getKoneksi()throws ClassNotFoundException, SQLException{
+       Class.forName("com.mysql.jdbc.Driver");
+       return DriverManager.getConnection(url,username,password);
+   
+   }
+}
